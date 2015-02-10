38bc8f537a2efc2cc7097f1e2bfd23a6d4a7bd91
Hart Russell <hrussell@digium.com>
2015-02-10 09:46:59 -0800

    Added git add -A to bash script

--- github.sh
+++ github.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+git add -A
 echo "Write commit message and press [ENTER]:"
 read commitmessage
-git commit -am "$commitmessage"
+git commit -m "$commitmessage"
 git push -u origin master
